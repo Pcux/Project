d7f87630428656fe6abc0903c5238bea167e062b
LostGhost1 <askbefore@mail.ru>
2019-01-13 11:35:31 +0300

    Added the "Guess Signs" screen and changed a bunch of small things

---
+++ app/src/main/java/com/example/belka/progaosnova/Guess_signsActivity.java
@@ -0,0 +1,18 @@
+package com.example.belka.progaosnova;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class Guess_signsActivity extends AppCompatActivity implements View.OnClickListener {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+    @Override
+    public void onClick(View v) {
+
+    }
+}
